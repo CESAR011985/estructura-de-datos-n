5746d679326336f869c746658b0535f45008faeb
CESAR011985 <jc.trujillog@uea.edu.ec>
2025-07-05 02:50:21 -0500

    semana02
    semana02

--- proyectos estructuta de datos/semana02/Program.cs
+++ proyectos estructuta de datos/semana02/Program.cs
@@ -33,26 +33,4 @@ public class Estudiante
     }
 }  
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        // Crear un estudiante  
-        string[] telefonosJulio = { "0991222367", "022345678", "0987654321" };
-        Estudiante julio = new Estudiante(
-            id: 1,
-            nombres: "Julio César",
-            apellidos: "Trujillo Gonzalez",
-            direccion: "otavalo calle jacinto collaguaso y luis alberto de la torre 110",
-            telefonos: telefonosJulio  // Ahora pasamos el array correctamente
-        );
-
-        // Mostrar datos  
-        julio.MostrarDatos();
-
-        // Registrar otro estudiante  
-        string[] telefonosMaria = { "0975643210", "022987654", "0998765432" };
-        Estudiante maria = new Estudiante(2, "Margarita", "Lomas", "otavalo Calle quiroga y 31 de octubre 812", telefonosMaria);
-        maria.MostrarDatos();
-    }
-}
+partial class Program  // <--
